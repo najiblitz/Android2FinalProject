a5bd0d1b35c4210b614cf278c3872f14c61e4626
Amanda Gayer <w0549527@myscc.ca>
2020-12-04 12:09:35 -0500

    add viewpager to New Products

--- app/src/main/java/com/example/android2finalproject/WhatsNewFragment.java
+++ app/src/main/java/com/example/android2finalproject/WhatsNewFragment.java
@@ -2,63 +2,52 @@ package com.example.android2finalproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WhatsNewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WhatsNewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WhatsNewFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WhatsNewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WhatsNewFragment newInstance(String param1, String param2) {
-        WhatsNewFragment fragment = new WhatsNewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_whats_new, container, false);
+        View view = inflater.inflate(R.layout.fragment_whats_new, container, false);
+        ((ViewPager)view.findViewById(R.id.viewpager)).setAdapter(new CustomViewPagerAdapter(getChildFragmentManager()));
+        return view;
+
+    }
+
+    private class CustomViewPagerAdapter extends FragmentPagerAdapter {
+
+        String[] descriptionContent = {"frruhfir","dfjiowejfioewf"};
+        int[] imageContent = {R.drawable.picture,R.drawable.ic_menu_camera};
+
+        public CustomViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if (position >= imageContent.length || position < 0) {
+                return viewPagerFragment.newInstance("error",0);
+            }
+            return viewPagerFragment.newInstance(descriptionContent[position],imageContent[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return imageContent.length;
+        }
     }
 }
